35f343b5dbbc27a63710f4adb24832f8134e0358
Andrew Howard <andrew@rootmypc.net>
2014-03-22 19:42:47 -0500

    Added check for curl binary

--- bashrc/bashrc.bash
+++ bashrc/bashrc.bash
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+ALLGOOD=1
+if which curl &>/dev/null -ne 0; then
+  echo "ERROR: curl binary not found in PATH"
+  ALLGOOD=0
+fi
+
+if [ $ALLGOOD -ne 1 ]; then
+  echo "SDK not loaded.  Correct problems and try again."
+  exit 1
+fi
+
 export BASHRC_BASE=$(pwd)/$(dirname $0)
 source $BASHRC_BASE/identity.bash
