e2aae16aacc2f7ec3f2cdb76a3b7eee735ec8c70
Andrew Howard <andrew@rootmypc.net>
2014-03-29 00:20:38 -0500

    Knocked off a couple todo items.  Explicit exit status and added endpoint helper scripts

--- bashrc/image.bash
+++ bashrc/image.bash
@@ -4,7 +4,8 @@ ABSPATH=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd && cd $OLDPWD)
 
 if [ -z "$BASHRC_BASE" ]; then
   echo "This script not intended to be executed directly."
-  echo "Please source bashrc.bash"
+  echo "Please source bashrc.bash, or at least define"
+  echo "$BASHRC_BASE manually."
   return 1
 fi
 
@@ -13,8 +14,8 @@ if [ -z "$BRCUTIL" ]; then
 fi
 
 # Hard-coded - fix this
-export BRC_REGION=$BRC_REGION
-export IMAGE_ENDPOINT="https://$BRC_REGION.images.api.rackspacecloud.com/v2"
+#export BRC_REGION=$BRC_REGION
+#export IMAGE_ENDPOINT="https://$BRC_REGION.images.api.rackspacecloud.com/v2"
 
 export PATH=$PATH:$BASHRC_BASE/bashrc/image
 
